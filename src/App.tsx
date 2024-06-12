698b8856b434bd08a94e7a4a01f7ed8a43375744
nishantvekariya1 <nishantvekariya623@gmail.com>
2024-06-12 11:34:42 +0530

    implement chart functionality

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,9 @@ const Transaction =  lazy(() => import('./pages/Transaction'))
 const NewProduct =  lazy(() => import('./pages/management/NewProduct'))
 const ProductManagement =  lazy(() => import('./pages/management/ProductManagement'))
 const TransactionManagement =  lazy(() => import('./pages/management/TransactionManagement'))
+const BarCharts =  lazy(() => import('./pages/charts/BarCharts'))
+const LineCharts =  lazy(() => import('./pages/charts/LineCharts'))
+const PieCharts =  lazy(() => import('./pages/charts/PieCharts'))
 
 const App = () => {
   return (
@@ -25,6 +28,9 @@ const App = () => {
             path="/admin/transaction/:id"
             element={<TransactionManagement />}
           />
+          <Route path="/admin/chart/bar" element={<BarCharts />} />
+          <Route path="/admin/chart/pie" element={<PieCharts />} />
+          <Route path="/admin/chart/line" element={<LineCharts />} />
       </Routes>
       </Suspense>
     </Router>
