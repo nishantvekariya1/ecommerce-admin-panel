3f4cd851d378977f1b203ae671854962062398fe
nishantvekariya1 <nishantvekariya623@gmail.com>
2024-07-08 15:08:16 +0530

    add screenshot of project

--- src/components/AdminSidebar.tsx
+++ src/components/AdminSidebar.tsx
@@ -1,53 +1,109 @@
-import { RiDashboardFill,RiShoppingBag3Fill,RiCoupon3Fill } from "react-icons/ri";
-import {AiFillFileText } from "react-icons/ai";
-import {IoIosPeople } from "react-icons/io";
-import {FaChartBar, FaChartPie, FaChartLine, FaStopwatch,FaGamepad} from "react-icons/fa";
-import { Link, useLocation, Location } from "react-router-dom"
+import { useEffect, useState } from "react";
 import { IconType } from "react-icons";
+import { AiFillFileText } from "react-icons/ai";
+import {
+  FaChartBar,
+  FaChartLine,
+  FaChartPie,
+  FaGamepad,
+  FaStopwatch,
+} from "react-icons/fa";
+import { HiMenuAlt4 } from "react-icons/hi";
+import { IoIosPeople } from "react-icons/io";
+import {
+  RiCoupon3Fill,
+  RiDashboardFill,
+  RiShoppingBag3Fill,
+} from "react-icons/ri";
+import { Link, Location, useLocation } from "react-router-dom";
 
 const AdminSidebar = () => {
-    const location = useLocation()
+  const location = useLocation();
+
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [phoneActive, setPhoneActive] = useState<boolean>(
+    window.innerWidth < 1100
+  );
+
+  const resizeHandler = () => {
+    setPhoneActive(window.innerWidth < 1100);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", resizeHandler);
+
+    return () => {
+      window.removeEventListener("resize", resizeHandler);
+    };
+  }, []);
+
   return (
-    <aside>
+    <>
+      {phoneActive && (
+        <button id="hamburger" onClick={() => setShowModal(true)}>
+          <HiMenuAlt4 />
+        </button>
+      )}
+
+      <aside
+        style={
+          phoneActive
+            ? {
+                width: "20rem",
+                height: "100vh",
+                position: "fixed",
+                top: 0,
+                left: showModal ? "0" : "-20rem",
+                transition: "all 0.5s",
+              }
+            : {}
+        }
+      >
         <h2>Logo.</h2>
         <DivOne location={location} />
         <DivTwo location={location} />
         <DivThree location={location} />
 
-    </aside>
-  )
-}
+        {phoneActive && (
+          <button id="close-sidebar" onClick={() => setShowModal(false)}>
+            Close
+          </button>
+        )}
+      </aside>
+    </>
+  );
+};
 
 const DivOne = ({ location }: { location: Location }) => (
-<div>
+  <div>
     <h5>Dashboard</h5>
     <ul>
-        <Li
-            url="/admin/dashboard"
-            text="Dashboard"
-            Icon={RiDashboardFill}
-            location={location} 
-            />
-        <Li
-            url="/admin/product"
-            text="Product"
-            Icon={RiShoppingBag3Fill}
-            location={location} 
-            />
-        <Li
-            url="/admin/customer"
-            text="Customer"
-            Icon={IoIosPeople}
-            location={location} 
-            />
-        <Li
-            url="/admin/transaction"
-            text="Transaction"
-            Icon={AiFillFileText}
-            location={location} 
-            />
+      <Li
+        url="/admin/dashboard"
+        text="Dashboard"
+        Icon={RiDashboardFill}
+        location={location}
+      />
+      <Li
+        url="/admin/product"
+        text="Product"
+        Icon={RiShoppingBag3Fill}
+        location={location}
+      />
+      <Li
+        url="/admin/customer"
+        text="Customer"
+        Icon={IoIosPeople}
+        location={location}
+      />
+      <Li
+        url="/admin/transaction"
+        text="Transaction"
+        Icon={AiFillFileText}
+        location={location}
+      />
     </ul>
-</div>
+  </div>
 );
 
 const DivTwo = ({ location }: { location: Location }) => (
@@ -128,4 +184,4 @@ const Li = ({ url, text, location, Icon }: LiProps) => (
   </li>
 );
 
-export default AdminSidebar
+export default AdminSidebar;
